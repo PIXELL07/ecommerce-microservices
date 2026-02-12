17be06c400dc015386c178bb98215fa8753b3135
PIXELL07 <arunksn007@gmail.com>
2026-02-13 00:32:05 +0530

    updates: product service

--- product-service/main.go
+++ product-service/main.go
@@ -1,1 +1,24 @@
-package productservice
+package main
+
+import (
+	// "os" import removed coz not used
+	"product-service/database"
+	"product-service/handlers"
+	"product-service/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	database.Connect()
+	database.DB.AutoMigrate(&models.Product{})
+
+	r := gin.Default()
+
+	r.POST("/products", handlers.CreateProduct)
+	r.GET("/products", handlers.GetProducts)
+	r.GET("/products/:id", handlers.GetProductByID)
+
+	r.Run(":8002")
+}
