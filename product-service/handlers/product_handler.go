6a2235e1d5be4f9eb2c6331389bc1a24bbf7a871
PIXELL07 <arunksn007@gmail.com>
2026-02-13 00:26:23 +0530

    product service

--- product-service/handlers/product_handler.go
+++ product-service/handlers/product_handler.go
@@ -1,1 +1,40 @@
 package handlers
+
+import (
+	"net/http"
+
+	"product-service/database"
+	"product-service/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateProduct(c *gin.Context) {
+	var product models.Product
+
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	database.DB.Create(&product)
+	c.JSON(http.StatusCreated, product)
+}
+
+func GetProducts(c *gin.Context) {
+	var products []models.Product
+	database.DB.Find(&products)
+	c.JSON(http.StatusOK, products)
+}
+
+func GetProductByID(c *gin.Context) {
+	id := c.Param("id")
+	var product models.Product
+
+	if err := database.DB.First(&product, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, product)
+}
